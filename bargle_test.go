8ef61ce58bc49c2ce48e56d67313a1d388d64fb3
Matt Joiner <anacrolix@gmail.com>
2022-06-21 18:53:02 +1000

    Update tests

--- bargle_test.go
+++ bargle_test.go
@@ -50,31 +50,31 @@ func TestUnmarshalPointer(t *testing.T) {
 	c := qt.New(t)
 	// Test unmarshalling in place
 	var b tagflag.Bytes
-	uf, err := mustGetUnaryUnmarshalAnyFunc(&b)
+	uf, err := makeAnyUnaryUnmarshalerViaReflection(&b)
 	c.Assert(err, qt.IsNil)
-	err = uf("1M")
+	err = uf.UnaryUnmarshal("1M")
 	c.Assert(err, qt.IsNil)
 	c.Check(b.Int64(), qt.Equals, int64(1_000_000))
 	// Unmarshal into nil pointer
 	var pb *tagflag.Bytes
-	uf, err = mustGetUnaryUnmarshalAnyFunc(&pb)
+	uf, err = makeAnyUnaryUnmarshalerViaReflection(&pb)
 	c.Assert(err, qt.IsNil)
-	err = uf("2M")
+	err = uf.UnaryUnmarshal("2M")
 	c.Assert(err, qt.IsNil)
 	c.Check(pb.Int64(), qt.Equals, int64(2_000_000))
 	// Check that reusing an unmarshal func doesn't adapt to a new pointer
 	npb := new(tagflag.Bytes)
 	pb = npb
-	err = uf("3M")
+	err = uf.UnaryUnmarshal("3M")
 	c.Assert(err, qt.IsNil)
 	c.Check(pb.Int64(), qt.Equals, int64(3_000_000))
 	c.Check(pb, qt.Not(qt.Equals), npb)
 	c.Check(npb.Int64(), qt.Equals, int64(0))
 	// Unmarshal into pointer to pointer
 	pab := new(tagflag.Bytes)
-	uf, err = mustGetUnaryUnmarshalAnyFunc(pab)
+	uf, err = makeAnyUnaryUnmarshalerViaReflection(pab)
 	c.Assert(err, qt.IsNil)
-	err = uf("4M")
+	err = uf.UnaryUnmarshal("4M")
 	c.Assert(err, qt.IsNil)
 	c.Check(pab.Int64(), qt.Equals, int64(4_000_000))
 }
